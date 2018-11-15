e5f1d155ebef4b4f804a4cb9472ca1408bff296c
VorSonnenaufgang <875503176@qq.com>
2018-11-15 18:54:51 +0800

    keyboard input

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,23 +1,21 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.input.*;
 import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-//			BorderPane root = new BorderPane();
-//			Scene scene = new Scene(root,400,400);
-//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-//			primaryStage.setScene(scene);
-//			primaryStage.show();
-            Parent root = FXMLLoader.load(getClass().getResource("calfx.fxml"));
+		try {			
+            Parent root = FXMLLoader.load(getClass().getResource("calfx.fxml"));       
             primaryStage.setTitle("¼ÆËãÆ÷");
             primaryStage.setScene(new Scene(root, 508, 604));
             primaryStage.setResizable(false);
--- src/application/calculator.java
+++ src/application/calculator.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.awt.Color;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -15,9 +16,15 @@ import java.util.regex.Pattern;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 
 /**
  * 可能出现的错误： 1.输入非法字符 2.括号不闭合 3.连续的操作符 4.
@@ -30,7 +37,37 @@ public class calculator implements Initializable {
 
 	@FXML
 	private ScrollPane scrollResult;
-
+	
+	@FXML
+	private Button Num0;
+	
+	@FXML
+	private Button Num1;
+	
+	@FXML
+	private Button Num2;
+	
+	@FXML
+	private Button Num3;
+	
+	@FXML
+	private Button Num4;
+	
+	@FXML
+	private Button Num5;
+	
+	@FXML
+	private Button Num6;
+	
+	@FXML
+	private Button Num7;
+	
+	@FXML
+	private Button Num8;
+	
+	@FXML
+	private Button Num9;
+	
 	
 	private static String preLines;
 	private static String curLine;
@@ -41,23 +78,6 @@ public class calculator implements Initializable {
 	 * 3.如果是*,/运算符，处理栈顶的所有*,/运算符，如果此时的栈顶的运算符是+，-那么直接入栈，处理完之后将提取出的运算符入栈
 	 * 4.如果是(,那么直接压入operatorStack栈中 5.如果是),重复处理来自operatorStack栈顶的运算符，知道看到栈顶的(
 	 */
-
-	
-	public String getCurLine() {
-		return curLine;
-	}
-	
-	public void setCurLine(String cur) {
-		curLine=cur;
-	}
-	
-	public String getPreLines() {
-		return preLines;
-	}
-	
-	public void setPreLines(String cur) {
-		preLines=cur;
-	}
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -202,5 +222,98 @@ public class calculator implements Initializable {
 
 		return source;
 	}
-
+	
+	public void press(KeyEvent event) {
+		KeyCode code=event.getCode();
+		switch(code) {
+		case DIGIT0:
+			curLine+="0";
+			textResult.setText(preLines + curLine);
+			Num0.setStyle("-fx-background-color: #D9D9D9;"); 
+			break;
+		case DIGIT1:
+			curLine+="1";
+			textResult.setText(preLines + curLine);
+			Num1.setStyle("-fx-background-color: #D9D9D9;"); 
+			break;
+		case DIGIT2:
+			curLine+="2";
+			textResult.setText(preLines + curLine);
+			Num2.setStyle("-fx-background-color: #D9D9D9;"); 
+			break;
+		case DIGIT3:
+			curLine+="3";
+			textResult.setText(preLines + curLine);
+			Num3.setStyle("-fx-background-color: #D9D9D9;"); 
+			break;
+		case DIGIT4:
+			curLine+="4";
+			textResult.setText(preLines + curLine);
+			Num4.setStyle("-fx-background-color: #D9D9D9;"); 
+			break;
+		case DIGIT5:
+			curLine+="5";
+			textResult.setText(preLines + curLine);
+			Num5.setStyle("-fx-background-color: #D9D9D9;"); 
+			break;
+		case DIGIT6:
+			curLine+="6";
+			textResult.setText(preLines + curLine);
+			Num6.setStyle("-fx-background-color: #D9D9D9;"); 
+			break;
+		case DIGIT7:
+			curLine+="7";
+			textResult.setText(preLines + curLine);
+			Num7.setStyle("-fx-background-color: #D9D9D9;"); 
+			break;
+		case DIGIT8:
+			curLine+="8";
+			textResult.setText(preLines + curLine);
+			Num8.setStyle("-fx-background-color: #D9D9D9;"); 
+			break;
+		case DIGIT9:
+			curLine+="9";
+			textResult.setText(preLines + curLine);
+			Num8.setStyle("-fx-background-color: #D9D9D9;"); 
+			break;
+		}
+		
+	}
+	
+	public void release(KeyEvent event) {
+		KeyCode code=event.getCode();
+		switch(code) {
+		case DIGIT0:
+			Num0.setStyle("-fx-background-color: #A9A9A9;"); 
+			break;
+		case DIGIT1:
+			Num1.setStyle("-fx-background-color: #A9A9A9;"); 
+			break;
+		case DIGIT2:
+			Num2.setStyle("-fx-background-color: #A9A9A9;"); 
+			break;
+		case DIGIT3:
+			Num3.setStyle("-fx-background-color: #A9A9A9;"); 
+			break;
+		case DIGIT4:
+			Num4.setStyle("-fx-background-color: #A9A9A9;"); 
+			break;
+		case DIGIT5:
+			Num5.setStyle("-fx-background-color: #A9A9A9;"); 
+			break;
+		case DIGIT6:
+			Num6.setStyle("-fx-background-color: #A9A9A9;"); 
+			break;
+		case DIGIT7:
+			Num7.setStyle("-fx-background-color: #A9A9A9;"); 
+			break;
+		case DIGIT8:
+			Num8.setStyle("-fx-background-color: #A9A9A9;"); 
+			break;
+		case DIGIT9:
+			Num9.setStyle("-fx-background-color: #A9A9A9;"); 
+			break;
+		}
+	}
+	
 }
